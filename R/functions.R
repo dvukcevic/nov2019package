410148640fd3d9c591ab6376fd319583d9b70339
Damjan Vukcevic <damjan.vukcevic@unimelb.edu.au>
2019-11-19 11:37:41 +1100

    First version

---
+++ R/functions.R
@@ -0,0 +1,25 @@
+#' Square the input
+#'
+#' This will square the input provided.
+#'
+#' @param x The input vector.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+newsquare <- function(x) {
+  x^2
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+newcube <- function(x) {
+  x^3
+}
